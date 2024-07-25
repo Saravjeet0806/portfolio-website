4a845febe94daee5cfa84e2e7d7eff1f735efcb0
Sarav <Saravjeetofficial@gmail.com>
2024-07-25 16:26:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='bg-red-300' >hello world</h1>
+    </>
+  )
+}
+
+export default App
