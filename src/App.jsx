16af929290b0248017ac54c7dcf8bf40d3af7a1f
Sarav <Saravjeetofficial@gmail.com>
2024-07-28 05:56:40 +0530

    added : about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import './App.css'
+import About from './components/About'
 
 function App() {
 
   return (
     <div className="mx-auto max-w-4xl">
       <Navbar/>
-      
+      <About/>
     </div>
   )
 }
