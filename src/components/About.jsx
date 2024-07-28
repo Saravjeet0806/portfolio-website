298ab4a95a8bfb963a9bb924475d25fd77ca5318
Sarav <Saravjeetofficial@gmail.com>
2024-07-28 06:16:03 +0530

    add : image in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import myImage from '../assets/myImage.png';
 
-function About() {
+const About = () => {
   return (
     <div>
-
-        
+      <div className="m-4 max-w-40 bg-slate-950">
+        <img src={myImage} alt="my-image" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
+
