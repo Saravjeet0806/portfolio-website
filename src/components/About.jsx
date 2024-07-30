9e5a179a291f3a65c43ad5ad27e2e3967969c61c
Sarav <Saravjeetofficial@gmail.com>
2024-07-30 17:40:01 +0530

    added : about me section details

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,17 @@ import myImage from '../assets/myImage.png';
 
 const About = () => {
   return (
-    <div>
-      <div className="m-4 max-w-40 bg-slate-950">
-        <img src={myImage} alt="my-image" />
-      </div>
+    <div className="flex flex-col items-center p-6 bg-gray-100 rounded-lg shadow-md md:flex-row md:items-start">
+    <div className="w-60 h-auto mb-4 md:mb-0 md:mr-6">
+      <img src={myImage} alt="my-image" className="w-full h-auto object-cover rounded-lg" />
     </div>
+    <div>
+      <h2 className="text-2xl font-bold mb-2 text-gray-800">About Me</h2>
+      <p className="text-gray-600 leading-relaxed">
+        Hi, I'm Saravjeet Singh, a 3rd-year BTech IT student with a passion for Data Structures and Algorithms, front-end web development, and DevOps. I have solved over 200 questions on LeetCode, attended Pragyaan (a soft skill program conducted by TP Cell GJUST), and maintained a good CGPA of 7+ each semester. I'm currently working on various projects, including a Weather App, a To-Do List App, and an MCQ App in React.
+      </p>
+    </div>
+  </div>
   );
 }
 
