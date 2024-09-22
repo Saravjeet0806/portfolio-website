8612eebcda14c15a753801c8436f9bcd53735698
Sarav <Saravjeetofficial@gmail.com>
2024-09-22 20:58:09 +0530

    added flex, text color and ul

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,11 @@ import Projects from './components/Projects'
 function App() {
 
   return (
-    <div className="mx-auto max-w-4xl">
+    <div >
       <Navbar/>
-      <About/>
-      <Skills/>
-      <Projects/>
+     
 
-      <footer className="App-footer p-4 bg-gray-800 text-white text-center">
-        <p>&copy; {new Date().getFullYear()} Saravjeet Singh. All rights reserved.</p>
-      </footer>
+     
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,19 @@ const Navbar = () => {
   }
   return (
     <div>
-      <div className='bg-black h-[100px]'>
+      <div className='bg-black text-gray-400 h-[100px] max-w-[1200px] mx-auto flex justify-between items-center'>
+
+        <h1 className='text-3xl font-bold primary-color ml-4'>Saravjeet Singh</h1>
+        <ul className='hidden md:flex'>
+          <li className='p-5'><a href='#about'>About</a></li>
+          <li className='p-5'><a href='#work'>Work</a></li>
+          <li className='p-5'><a href='#contact'>Contact</a></li>
+        </ul>
 
 
 
         <div onClick={handleNav} className='block md:hidden'>
-          {nav?<AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
         </div>
       </div>
     </div>
