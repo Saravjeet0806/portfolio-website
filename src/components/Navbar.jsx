203818c04938976cd155d79677214838e59971e4
Sarav <Saravjeetofficial@gmail.com>
2024-09-22 20:45:06 +0530

    added handle Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,20 @@ import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const handleNav = () =>{
+    setNav(!nav)
+  }
   return (
+    <div>
+      <div className='bg-black h-[100px]'>
+
+
+
+        <div onClick={handleNav} className='block md:hidden'>
+          {nav?<AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+        </div>
+      </div>
+    </div>
    
   )
 };
