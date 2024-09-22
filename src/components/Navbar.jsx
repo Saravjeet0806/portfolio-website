1aa605d9914d0a7516a1405f0de3b05ae4141a25
Sarav <Saravjeetofficial@gmail.com>
2024-09-22 20:40:54 +0530

    modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
   return (
-    <nav className="bg-gray-700 p-4">
-      <div className="container flex justify-between items-center">
-        <div className="text-white text-3xl">My Portfolio</div>
-        <div className="flex justify-center flex-grow space-x-4">
-          <a href="#About" className="text-white hover:text-red-500 mx-2 text-xl">About</a>
-          <a href="#Skills" className="text-white hover:text-red-500 mx-2 text-xl">Skills</a>
-          <a href="#Projects" className="text-white hover:text-red-500 mx-2 text-xl">Projects</a>
-        </div>
-      </div>
-    </nav>
-  );
+   
+  )
 };
 
 export default Navbar;
