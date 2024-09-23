db3382ddb6d07166eadd8d78751005cf82f697d5
Sarav <Saravjeetofficial@gmail.com>
2024-09-23 21:42:03 +0530

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,12 +16,19 @@ const Navbar = () => {
           <li className='p-5'><a href='#about'>About</a></li>
           <li className='p-5'><a href='#work'>Work</a></li>
           <li className='p-5'><a href='#contact'>Contact</a></li>
-        </ul>
+        </ul> 
 
-
-
-        <div onClick={handleNav} className='block md:hidden'>
+        <div onClick={handleNav} className='block md:hidden mr-5'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+        </div>
+
+        <div className={nav ? 'fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <h1 className='text-3xl font-bold primary-color ml-4'>Saravjeet Singh</h1>
+          <ul className='p-8 text-2xl'>
+          <li className='p-2'><a href='#about'>About</a></li>
+          <li className='p-2'><a href='#work'>Work</a></li>
+          <li className='p-2'><a href='#contact'>Contact</a></li>
+          </ul>
         </div>
       </div>
     </div>
