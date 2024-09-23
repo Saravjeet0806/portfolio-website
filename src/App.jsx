16f40fde5664cf775da4899a4c64ab6cfa7ab282
Sarav <Saravjeetofficial@gmail.com>
2024-09-23 21:52:47 +0530

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import './App.css'
 import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
+import Hero from './components/Hero'
 
 function App() {
 
   return (
     <div >
       <Navbar/>
-     
+     <Hero/>
 
      
     </div>
