bc7b4cbf72d85b5caf41f661f88e92bea77ca813
Sarav <Saravjeetofficial@gmail.com>
2024-09-24 21:20:02 +0530

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Image from '../assets/myImage.png'
+import { TypeAnimation} from 'react-type-animation'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] mx-auto py-8 bg-black'> 
+    
+    hero section
+    
+    </div>
   )
 }
 
