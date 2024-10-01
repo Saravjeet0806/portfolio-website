3d29f478a1a21821f65f9543631f7607caba050b
Sarav <Saravjeetofficial@gmail.com>
2024-10-01 13:54:13 +0530

    added image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,13 @@ const Hero = () => {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] mx-auto py-8 bg-black'> 
     
-    hero section
+      <div className='my-auto mx-auto'>
+        <div className='w-[300px] h-auto lg:w-[400px]'>
+            <img src={Image} alt="saravjeet image" />
+        </div>
+      </div>
+
+      <div className='col-span-2 px-5'></div>
     
     </div>
   )
