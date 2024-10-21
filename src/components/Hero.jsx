184e963c570dbe0a47c196296caed87be63f30d4
Sarav <Saravjeetofficial@gmail.com>
2024-10-21 20:26:38 +0530

    added animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,16 @@ const Hero = () => {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] mx-auto py-8 bg-black'> 
     
-      <div className='my-auto mx-auto'>
-        <div className='w-[300px] h-auto lg:w-[400px]'>
+      <div className='col-span-1 my-auto mx-auto w-[200px] h-auto lg:w-[300px]'>
             <img src={Image} alt="saravjeet image" />
-        </div>
       </div>
-
       <div className='col-span-2 px-5'></div>
-    
+
+      <h1 className='text-white text-4xl sm:5xl lg:text-8xl font-extrabold'>
+       <span className='primary-color'>I'm a </span> <br/>
+       <TypeAnimation sequence={["Frontend Developer", 1000, "DevOps Engineer", 1000]} 
+       wrapper="span" speed={50} repeat={Infinity}/>
+    </h1>
     </div>
   )
 }
