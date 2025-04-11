8b8ec074e5e750395fe6b5cb8f5314b10086f269
Sarav <Saravjeetofficial@gmail.com>
2025-04-11 14:13:12 +0530

    hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
 import Image from '../assets/myImage.png'
-import { TypeAnimation} from 'react-type-animation'
+import { TypeAnimation } from 'react-type-animation'
 
 const Hero = () => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] mx-auto py-8 bg-black'> 
-    
-      <div className='col-span-1 my-auto mx-auto w-[200px] h-auto lg:w-[300px]'>
-            <img src={Image} alt="saravjeet image" />
-      </div>
-      <div className='col-span-2 px-5'></div>
+    <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] mx-auto py-8 bg-black'>
 
-      <h1 className='text-white text-4xl sm:5xl lg:text-8xl font-extrabold'>
-       <span className='primary-color'>I'm a </span> <br/>
-       <TypeAnimation sequence={["Frontend Developer", 1000, "DevOps Engineer", 1000]} 
-       wrapper="span" speed={50} repeat={Infinity}/>
-    </h1>
+      <div className='col-span-1 my-auto mx-auto w-[200px] h-auto lg:w-[300px]'>
+        <img src={Image} alt="saravjeet image" />
+      </div>
+      <div className='col-span-2 px-5'>
+
+        <h1 className='text-white text-4xl sm:5xl lg:text-8xl font-extrabold'>
+          <span className='primary-color'>I'm a </span> <br />
+          <TypeAnimation sequence={["Frontend Developer", 1000, "DevOps Engineer", 1000]}
+            wrapper="span" speed={50} repeat={Infinity} />
+        </h1>
+        <p className='text-white sm:text-lg my-6 lg:text-xl'>My name is Saravjeet Singh and I am a passionate Devops Engineer and Web Developer</p>
+        <div className="my-8">
+          <a
+            href="/"
+            className="px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-br from-orange-500 to-pink-500 text-white"
+          >
+            Download CV
+          </a>
+          <a href="#contact"
+            className="px-6 py-3 w-full rounded-xl  border border-gray-400 hover:bg-gradient-to-br from-orange-500 to-pink-500 text-white hover:border-none"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
