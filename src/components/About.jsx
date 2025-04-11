81ba64002d4b413aeae3b999e43347f5c1b0e302
Sarav <Saravjeetofficial@gmail.com>
2025-04-11 20:40:29 +0530

    finalized about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,19 +10,25 @@ const About = () => {
             About Me
           </h2>
           <p className="text-lg leading-relaxed mb-6 text-gray-300">
-            Hi, I’m <span className="text-white font-semibold">Saravjeet Singh</span>, a driven <span className="text-pink-400 font-semibold">DevOps Engineer</span> and <span className="text-orange-400 font-semibold">Frontend Developer</span>. Currently pursuing my B.Tech in IT from GJUS&T, I thrive on building smart, scalable, and impactful tech solutions.
+            Hi, I’m <span className="text-white font-semibold">Saravjeet Singh</span>, a driven <span className="text-pink-400 font-semibold">DevOps Engineer</span> and <span className="text-orange-400 font-semibold">Frontend Developer</span>. Currently pursuing my B.Tech in Information Technology from GJUS&T, I am a 3rd Year student with a keen interest in technology and Software Development.
           </p>
           <p className="text-lg leading-relaxed mb-6 text-gray-300">
             I’m hands-on with tools like <span className="text-white font-medium">Docker, Kubernetes, Ansible, and Terraform</span>, helping automate cloud infrastructure and CI/CD pipelines. On the frontend side, I craft beautiful interfaces using <span className="text-white font-medium">React.js, Tailwind CSS</span>, and the <span className="text-white font-medium">MERN stack</span>.
           </p>
           <p className="text-lg leading-relaxed mb-6 text-gray-300">
-            I actively participate in coding competitions, mentor juniors, and love contributing to tech communities. Whether it’s shipping products or speaking on stage—I'm all about learning and leveling up.
+          I’m always exploring new technologies, continuously learning, and open to exciting collaborations in software development and DevOps. Let’s connect and build something impactful together!
           </p>
           <a
             href="/#projects"
             className="inline-block mt-4 px-6 py-3 rounded-xl bg-gradient-to-br from-orange-500 to-pink-500 text-white hover:opacity-90 transition"
           >
             View My Work
+          </a>
+          <a
+            href="https://github.com/Saravjeet0806"
+            className="inline-block mt-4 px-6 py-3 rounded-xl bg-gradient-to-br  text-white hover:opacity-90 transition"
+          >
+            Github
           </a>
         </div>
 
